86492894d96158b6d1f66541c6d4aa491989cd01
c23027063 <roch-joel.CUBAHIRO@etu.univ-amu.fr>
2025-03-24 09:53:27 +0100

    api working

--- AppTest/backend/Controllers/match.controller.js
+++ AppTest/backend/Controllers/match.controller.js
@@ -1,7 +1,35 @@
 import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-export const getMatches = async (req, res) => {
-    try {
-        
-    }
+const API_KEY = process.env.API_KEY;
+
+export const getplayer = async (req, res) => {
+  //const { id } = req.params;
+  const URL = "https://v3.football.api-sports.io/players/squads";
+  const config = {
+    method: "GET",
+    url: URL,
+    params: { team: "33" },
+    headers: {
+      "x-rapidapi-key": API_KEY,
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    },
+  };
+
+  /*  axios.get(URL, config).then((response) => {
+    console.log(response.data);
+    res.json(data);
+  });*/
+
+  try {
+    const response = await axios.get(URL, config);
+    //console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching players:", error);
+    res.status(500).json({ error: "Failed to fetch players" });
+  }
+};
--- AppTest/backend/routers/server.js
+++ AppTest/backend/routers/server.js
@@ -2,24 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import axios from "axios";
 import path from "path";
-import cors from '"';
+import cors from "cors";
+
+//functions
+import { getplayer } from "../Controllers/match.controller.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running at http://localhost:", PORT);
 });
 
-/* const express = require("express"); // Sets up the Express web server.
-const cors = require("cors"); //Enables Cross-Origin Resource Sharing (CORS), allowing API requests from different origins.
-const axios = require("axios"); //HTTP client for making API requests (e.g., fetching data from an external API)
-const path = require("path"); //Helps with file and directory paths
-require("dotenv").config(); //Loads environment variables from a .env file (useful for API keys, database credentials, etc.).
-
-const app = express();
-const PORT = process.env.PORT || 5000;
+app.get("/api/player", getplayer);
+app.get("api/match");
 
 // Middleware
 app.use(cors()); // Enables CORS, allowing frontend apps from different domains to access this backend.
