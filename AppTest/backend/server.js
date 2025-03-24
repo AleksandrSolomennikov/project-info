a008eb985945974d661a7104c988bd24df235a12
Aleksandr Solomennikov <aleksandr.solomennikov@etu-univ.amu.com>
2025-03-24 09:53:44 +0100

    small commit

--- AppTest/backend/server.js
+++ AppTest/backend/server.js
@@ -33,12 +33,5 @@ app.get("/api/matches", async (req, res) => {
 });
 
 //Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
